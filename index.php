16d18b7cf8854c99bdc6e77f09ff98b169018f3c
codeKID <acuaytv@gmail.com>
2018-05-02 16:42:58 -0700

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+    <script src="https://api.trello.com/1/client.js?key=840d3f173ffaa4498c67ca64bf83d2d9"></script>
+    <style src="style/main.css" type="text/css"></style>
+    <title>Toastio.  Easy Project Management</title>
+  </head>
+  <body>
+    <div class="topnav">
+      <a class="active" href="/">
+    </div>
+  </body>
+</html>
